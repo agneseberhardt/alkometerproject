cb50f080862b1cb163b69e45505370f0dfcc474e
Unknown <unknown@example.com>
2021-02-08 19:09:35 +0200

    project works

--- alko/App.js
+++ alko/App.js
@@ -1,59 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState}from 'react';
-import {Picker} from '@react-native-community/picker';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { Picker } from '@react-native-community/picker';
+import RadioForm from 'react-native-simple-radio-button';
 
 export default function App() {
-  const [weight, setWeight] = useState(0);
-  const [bottles, setBottles] = useState(1);
-  const [time, setTime] = useState(1)
+
+  var radio_props = [
+    { label: 'Male', value: 'male' },
+    { label: 'Female', value: 'female' }
+  ];
+
+
+
+  const [weight, setweight] = useState('');
+  const [time, settime] = useState(1);
+  const [bottles, setbottles] = useState(1);
+  const [gender, setgender] = useState('male');
+  const [promiles, setpromiles] = useState(0);
+
+  function calculate(){
+    let promiles = 0;
+    let liters = bottles * 0.33;
+    let grams = liters * 8 * 4.5;
+    let burning = weight / 10;
+    let gramsL = grams - burning * time;
+
+    
+    if (gender == 'male') {
+      promiles = gramsL / (weight * 0.7);
+     
+
+
+    }else if (gender == 'female'){
+      promiles = gramsL / (weight * 0.6);
+
+    }
+    setpromiles(promiles)
+  }
+
+
   return (
-    <View style={styles.container}>
+    <View style={styles.top}>
+      <View >
+        <Text>Weight</Text>
+        <TextInput  onChangeText={value => setweight(value)} placeholder="enter weight" keyboardType='numeric'></TextInput>
+      </View>
+      <View style={{ backgroundColor: '#fafafa' }}>
+        <Text>Bottles</Text>
+        <Picker
+          selectedValue={bottles}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue, itemIndex) =>
+            setbottles(itemValue)
+          }>
+          <Picker.Item label="1 bottles" value={1}/>
+          <Picker.Item label="2 bottles" value={2}/>
+          <Picker.Item label="3 bottles" value={3}/>
+          <Picker.Item label="4 bottles" value={4}/>
+          <Picker.Item label="5 bottles" value={5}/>
+          <Picker.Item label="6 bottles" value={6}/>
+          <Picker.Item label="7 bottles" value={7}/>
+          <Picker.Item label="8 bottles" value={8}/>
+          <Picker.Item label="9 bottles" value={9}/>
+          <Picker.Item label="10 bottles" value={10}/>
+        </Picker>
+      </View>
+
+      <View style={{ backgroundColor: '#fafafa' }}>
+        <Text >Time</Text>
+        <Picker
+          selectedValue={time}
+          style={{ height: 50, width: 150 }}
+          onValueChange={(itemValue, itemIndex) =>
+            settime(itemValue)
+          }>
+          <Picker.Item label="1 hour" value={1}/>
+          <Picker.Item label="2 hours" value={2}/>
+          <Picker.Item label="3 hours" value={3}/>
+          <Picker.Item label="4 hours" value={4}/>
+          <Picker.Item label="5 hours" value={5}/>
+          <Picker.Item label="6 hours" value={6}/>
+          <Picker.Item label="7 hours" value={7}/>
+          <Picker.Item label="8 hours" value={8}/>
+          <Picker.Item label="9 hours" value={9}/>
+          <Picker.Item label="10 hours" value={10}/>
+
+
+        </Picker>
+      </View>
       <View>
-     <Text>Weight</Text>
-     <TextInput onChangeText={value => setWeight(value)} placeholder='weight' keyboardType='numeric'></TextInput>
-     </View>
-     <View>
-     <Text>bottles</Text>
-     <Picker
-  selectedValue={bottles}
-  style={{height: 50, width: 200}}
-  onValueChange={(itemValue, itemIndex) =>
-    setBottles({itemValue})
-  }>
-  <Picker.Item label="1 bottle" value={1} />
-  <Picker.Item label="2 bottles" value={2} />
-  <Picker.Item label="3 bottles" value={3} />
-  <Picker.Item label="4 bottles" value={4} />
-  <Picker.Item label="5 bottles" value={5} />
-  <Picker.Item label="6 bottles" value={6} />
-</Picker>
-     </View>
-     <View>
-       <Text>Time</Text>
-     <Picker
-  selectedValue={time}
-  style={{height: 50, width: 200}}
-  onValueChange={(itemValue, itemIndex) =>
-    setTime({itemValue})
-  }>
-  <Picker.Item label="1 hour" value={1} />
-  <Picker.Item label="2 hours" value={2} />
-  <Picker.Item label="3 hours" value={3} />
-  <Picker.Item label="4 hours" value={4} />
-  <Picker.Item label="5 hours" value={5} />
-  <Picker.Item label="6 hours" value={6} />
-</Picker>
-     </View>
+        <Text >gender</Text>
+        <RadioForm
+          radio_props={radio_props}
+          initial={0}
+          onPress={(value) => setgender(value)}
+        >
+        </RadioForm>
+      </View>
+
+      <View>
+        <Button
+          title="calculate"
+          onPress={calculate}
+        />
+      </View>
+      <View>
+        <Text>Promiles</Text>
+        <Text>{promiles.toFixed(2)}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    padding:10
   },
+  top:{
+    marginTop:100
+
+  }
 });
